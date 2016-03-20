574816cce2a37a180f067cd8507d812fab11df31
jameswyper <jrwyper@yahoo.co.uk>
2016-03-20 12:56:19 +0000

    First thoughts

---
+++ UPnPBase.rb
@@ -0,0 +1,20 @@
+
+class UPnPDevice
+def createUUID
+end
+def sendSSDP
+end
+def sendSSDPbyebye
+end
+@services=Array.new
+end
+
+
+class UPnPRootDevice < UPnPDevice
+
+
+	
+end
+
+class UPnPService
+end
---
+++ rupture.rb
@@ -0,0 +1,2 @@
+
+require 'UPnPBase.rb'
