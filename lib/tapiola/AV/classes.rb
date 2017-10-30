4ca2f63f7c2bb886d3130d47663fc5e20524c161
James Wyper <jrwyper@yahoo.co.uk>
2017-10-30 22:04:57 +0000

    bit more AV work

--- lib/tapiola/AV/classes.rb
+++ lib/tapiola/AV/classes.rb
@@ -18,35 +18,46 @@ class Property
 					:refID => Property.new(:parentID,:attribute,:DIDL),
 					:restricted => Property.new(:restricted,:attribute,:DIDL),
 					:writeStatus => Property.new(:writeStatus, :element, :UPnP),
-					:childCount => Property.new(:childCount, :attribute, :DIDL)
-				}
+					:childCount => Property.new(:childCount, :attribute, :DIDL),
+					:storageMedium => Property.new(:storageMedium,:element,:UPnP), 
+					:longDescription => Property.new(:longDescription,:element,:UPnP), 
+					:description => Property.new(:description,:element,:DC), 
+					:publisher => Property.new(:publisher, :element, :DC), 
+					:contributor => Property.new(:contributor,:element, :DC), 
+					:date => Property.new(:date,:element,:DC), 
+					:relation => Property.new(:relation,:element,:DC), 
+					:rights => Property.new(:rights,:element,:DC), 
+					:artist => Property.new(:artist,:element,:UPnP), 
+					:genre => Property.new(:genre, :element, :UPnP), 
+					:producer => Property.new(:producer,:element,:UPnP), 
+					:albumArtURI => Property.new(:albumArtURI,:element,:UPnP), 
+					:toc => Property.new(:toc,:element,:UPnP)
+			}
 end
 
 
 class CDObject
 	
-	def properties
-		{  	:id => [true,false], :parentID => [true,false], :title => [true,false], 
-					:creator => [false,false], :res => [false,true], :class => [true,false], 	
-					:restricted => [true,false],:writeStatus => [false,false] }
-	end
+	attr_reader :properties
 	def requiredProperty?(prop)
-		return properties[prop][0]
+		return @properties[prop][0]
 	end
 	def multiValuedProperty?(prop)
-		return properties[prop][1]
+		return @properties[prop][1]
 	end
 	def initialize
 		@parent = nil
+		@properties = 		{  	:id => [true,false], :parentID => [true,false], :title => [true,false], 
+					:creator => [false,false], :res => [false,true], :class => [true,false], 	
+					:restricted => [true,false],:writeStatus => [false,false] }
+
 	end
 end
 
 class CDItem < CDObject
-	def properties
-		super.merge( { :refID => [false,false] })
-	end
 	def initialize
 		super
+		@properties.merge!( { :refID => [false,false] })
 	end
 	def linkToParent(p)
 		@parent = p
@@ -58,12 +69,10 @@ class CDItem < CDObject
 end
 
 class CDContainer < CDObject
-	def properties
-		super.merge({ :childCount => [false,false], :createClass => [false,true], :searchClass => [false,true], :searchable => [false,false]})
-	end
 	def initialize
 		super
 		@children = Array.new
+		@properties.merge!({ :childCount => [false,false], :createClass => [false,true], :searchClass => [false,true], :searchable => [false,false]})
 	end
 	def addChild(c)
 		@children << c
@@ -75,7 +84,25 @@ class CDContainer < CDObject
 	end
 end
 
+class CDAlbum < CDContainer
+	def initialize
+		super
+		@properties.merge!({ :storageMedium => [false,false], :longDescription => [false,false], :description => [false,false], 
+		:publisher => [false,true], :contributor => [false,true], :date => [false,false], :relation => [false,true], :rights => [false,true] })
+	end
+end
+
+class CDMusicAlbum < CDAlbum
+	def initialize
+		super
+		@properties.merge!({ :artist => [false,true], :genre => [false,true], :producer => [false,true], :albumArtURI => [false,true], :toc => [false,false] })
+	end
+end
+
 end #module
 
 x = AV::CDContainer.new
+puts x.requiredProperty?(:id)
 puts x.requiredProperty?(:childCount)
+
+
