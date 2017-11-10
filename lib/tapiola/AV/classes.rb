9fabc44c9647d9bf2479bd62d7dc01240cc45d47
James Wyper <jrwyper@yahoo.co.uk>
2017-11-10 23:01:47 +0000

    thinking through the ContentDirectory service stuff

--- lib/tapiola/AV/classes.rb
+++ lib/tapiola/AV/classes.rb
@@ -1,108 +1,169 @@
 module AV
 
-class Property
-	def initialize(name,xmltype,namespace)
-		@name = name
-		@xmlType = xmltype
-		@namespace = namespace
-	end
-	@@Properties = { 	:id => Property.new(:id,:attribute,:DIDL),
-					:title => Property.new(:title,:element,:DC),
-					:creator => Property.new(:creator,:element,:DC),
-					:res => Property.new(:res,:element,:DIDL),
-					:class => Property.new(:class,:element,:UPnP),
-					:searchable => Property.new(:searchable,:attribute,:UPnP),
-					:searchClass => Property.new(:searchClass, :element, :UPnP),
-					:createClass => Property.new(:createClass, :element, :UPnP),
-					:parentID => Property.new(:parentID, :attribute,:DIDL),
-					:refID => Property.new(:parentID,:attribute,:DIDL),
-					:restricted => Property.new(:restricted,:attribute,:DIDL),
-					:writeStatus => Property.new(:writeStatus, :element, :UPnP),
-					:childCount => Property.new(:childCount, :attribute, :DIDL),
-					:storageMedium => Property.new(:storageMedium,:element,:UPnP), 
-					:longDescription => Property.new(:longDescription,:element,:UPnP), 
-					:description => Property.new(:description,:element,:DC), 
-					:publisher => Property.new(:publisher, :element, :DC), 
-					:contributor => Property.new(:contributor,:element, :DC), 
-					:date => Property.new(:date,:element,:DC), 
-					:relation => Property.new(:relation,:element,:DC), 
-					:rights => Property.new(:rights,:element,:DC), 
-					:artist => Property.new(:artist,:element,:UPnP), 
-					:genre => Property.new(:genre, :element, :UPnP), 
-					:producer => Property.new(:producer,:element,:UPnP), 
-					:albumArtURI => Property.new(:albumArtURI,:element,:UPnP), 
-					:toc => Property.new(:toc,:element,:UPnP)
-			}
+class CDSetupError < ::StandardError
 end
 
-
-class CDObject
+class CDClass
+	PropertyMeta = Struct.new(:xmlType, :namespace, :required, :multiValued)
+#
+#  set up the properties for the base Content Directory Object in a hash
+#	
+	@@properties = {
+					:CDObject => 
+					{ 	:id => PropertyMeta.new( :attribute, :DIDL, true, false),
+						:parentID => PropertyMeta.new(:attribute, :DIDL, true, false),
+						:title => PropertyMeta.new(:element,:DC,true,false),
+						:creator => PropertyMeta.new(:element,:DC,false,false),
+						:res => PropertyMeta.new(:element, :DIDL,false,true),
+						:class => PropertyMeta.new(:element, :UPnP,true,false),
+						:restricted => PropertyMeta.new(:attribute, :DIDL,true,false),
+						:writeStatus => PropertyMeta.new(:element, :UPnP,false,false)
+					}
+				}
+#				
+#  set up the properties for derived objects by adding a new entry to the top level hash 
+#  that combines the properties of the new class and the class it is derived from
+#
+	@@properties.merge! ({ :CDItem => @@properties[:CDObject].merge(
+					{
+					:refID => PropertyMeta.new(:attribute,:DIDL,false,false),
+					}
+					)  })
+# and so on..					
+	@@properties.merge! ({ :CDContainer => @@properties[:CDObject].merge(
+					{
+					:childCount => PropertyMeta.new(:attribute,:DIDL,false,false),
+					:createClass => PropertyMeta.new(:element,:UPnP,false,true),
+					:searchClass => PropertyMeta.new(:element,:UPnP,false,true),
+					:searchable => PropertyMeta.new(:attribute,:UPnP,false,false)
+					}
+					)  })
+	@@properties.merge! ({ :CDAlbum => @@properties[:CDContainer].merge(
+					{
+					:storageMedium => PropertyMeta.new(:element,:UPnP,false,false),
+					:longDescription => PropertyMeta.new(:element,:UPnP,false,false),
+					:description => PropertyMeta.new(:element,:DC,false,false),
+					:publisher => PropertyMeta.new(:element, :DC,false,true),
+					:contributor => PropertyMeta.new(:element, :DC,false,true),
+					:date => PropertyMeta.new(:element, :DC,false,false),
+					:relation => PropertyMeta.new(:element, :DC,false,true),
+					:rights => PropertyMeta.new(:element, :DC,false,true)
+					}
+					)  })
+	@@properties.merge! ({ :CDMusicAlbum => @@properties[:CDAlbum].merge(
+					{
+					:artist => PropertyMeta.new(:element,:UPnP,false,true),
+					:genre => PropertyMeta.new(:element,:UPnP,false,true),
+					:producer => PropertyMeta.new(:element,:UPnP,false,true),
+					:albumArtURI => PropertyMeta.new(:element,:UPnP,false,true),
+					:toc => PropertyMeta.new(:element,:UPnP,false,false)
+					}
+					)  })
+	@@classes = { :CDItem => :item, :CDContainer => :container, :CDAlbum => :container, :CDMusicAlbum => :container }
 	
-	attr_reader :properties
-	def requiredProperty?(prop)
-		return @properties[prop][0]
-	end
-	def multiValuedProperty?(prop)
-		return @properties[prop][1]
-	end
-	def initialize
-		@parent = nil
-		@properties = 		{  	:id => [true,false], :parentID => [true,false], :title => [true,false], 
-					:creator => [false,false], :res => [false,true], :class => [true,false], 	
-					:restricted => [true,false],:writeStatus => [false,false] }
 
+	attr_reader :type
+	attr_reader :property
+	
+	def initialize(classname, parent)
+		@property = Array.new
+		@classname = classname
+		if (@type = @@classes[classname] == nil)
+			raise CDSetupError, "class #{classname} undefined"
+		end
+		if (@type == :container)
+			@childItems = Array.new
+			@childContainers = Array.new
+		end
+		@parent = parent
+		if (parent) then @parent.addChild[self] end
 	end
-end
-
-class CDItem < CDObject
-	def initialize
-		super
-		@properties.merge!( { :refID => [false,false] })
+	
+	def children
+		return @childContainers + @childItems
 	end
-	def linkToParent(p)
-		@parent = p
-		p.addChild(self)
+	
+	def addChild(child)
+		if child.type == :container
+			@childContainers << child
+		else
+			@childItems << child
+		end
 	end
-	def setParent(p)
-		@parent = p
+	
+	def removeChild(child)
+		if child.type == :container
+			@childContainers.delete(child)
+		else
+			@childItems.delete(child)
+		end
 	end
-end
-
-class CDContainer < CDObject
-	def initialize
-		super
-		@children = Array.new
-		@properties.merge!({ :childCount => [false,false], :createClass => [false,true], :searchClass => [false,true], :searchable => [false,false]})
+	
+	def addProperty(name,value)
+		p = @@properties[@classname][name]
+		if p
+			@property[name] = value
+		else
+			raise CDSetupError, "Property #{name} not defined for class #{@classname}"
+		end
 	end
-	def addChild(c)
-		@children << c
-		c.setParent(self)
+	
+	def checkproperties
+		ok = true
+		missing = Array.new
+		required = Hash.new
+		@@properties[@classname].each do |k,v|
+			if v.required
+				required[k] = false
+			end
+		end
+		@property.each do |k,v|
+			if required[k] != nil
+				required[k] = true
+			end
+		end
+		required.each do  |k,v|
+			ok = ok && v
+			if (!v)
+				missing << k
+			end
+		end
+		if !ok
+			raise CDSetupError, "required properties #{missing.to_s} missing for #{@classname}"
+		end
 	end
-	def removeChild(c)
-		c.setParent(nil)
-		@children.delete(c)
+	
+	def createXMLFragment(filter)
 	end
-end
-
-class CDAlbum < CDContainer
-	def initialize
-		super
-		@properties.merge!({ :storageMedium => [false,false], :longDescription => [false,false], :description => [false,false], 
-		:publisher => [false,true], :contributor => [false,true], :date => [false,false], :relation => [false,true], :rights => [false,true] })
+	
+		
+	def self.getChildren(object,sort,offset,limit)
+		if (@@cacheObject != object || @@cacheSort != sort)
+			@@cacheChildren = getAllChildren(object,sort)
+		end
+		s = @@cacheChildren.size
+		if (offset > s)
+			return Array.new
+		end
+		if ((offset + limit - 1) > s)
+			return @@cacheChildren[offset..s]
+		else
+			return @@cacheChildren[offset..(offset + limit -1)]
+		end
 	end
-end
-
-class CDMusicAlbum < CDAlbum
-	def initialize
-		super
-		@properties.merge!({ :artist => [false,true], :genre => [false,true], :producer => [false,true], :albumArtURI => [false,true], :toc => [false,false] })
+	
+	def self.getAllChildren(object,sort)
+		@@cacheObject = object
+		@@cacheSort = sort
+		#decide whether we're getting containers or items or both
+		# get them
+		# sort them
+		# store them
 	end
+	
 end
 
 end #module
 
-x = AV::CDContainer.new
-puts x.requiredProperty?(:id)
-puts x.requiredProperty?(:childCount)
 
 
