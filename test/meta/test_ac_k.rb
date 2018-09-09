881c79b5648083c0461fb3dfeb540b09a686073a
James Wyper <jrwyper@yahoo.co.uk>
2018-09-09 20:59:39 +0100

    config

--- test/meta/test_ac_k.rb
+++ test/meta/test_ac_k.rb
@@ -13,13 +13,15 @@ require_relative '../../lib/tapiola/Metadata/metacore.rb'
 		#puts "Got release OK #{dummy.title}"
 		
 		tf = Meta::Core::Folder.new('/home/james/Music/flac/classical')
+		Meta::Core::DBBase.beginLUW	
 		tf.scan { |count,total,eta| puts "#{sprintf('%2.1f',(total == 0 ? 100.0 : (count * 100.0) / total))}% complete, ETC #{eta.strftime('%b-%d %H:%M.%S')}"}
-		
+		Meta::Core::DBBase.endLUW	
 		ac = Meta::AcoustID::Service.new('fpcalc')
 		
 		discs = tf.fetchDiscs
 		count = 0
 		discs.each do |disc|
+			Meta::Core::DBBase.beginLUW	
 			count = count + 1
 			scores = ac.scoreDisc(disc)
 			puts "#{count} of #{discs.size}: #{disc.pathname}/#{disc.discNumber}"
@@ -28,6 +30,7 @@ require_relative '../../lib/tapiola/Metadata/metacore.rb'
 					puts "#{s.release.title}/#{s.release.mbid}/#{s.medium.position} #{s.trackCount}/#{s.trackMatches}/#{s.trackMisses}"
 				end
 			end
+			Meta::Core::DBBase.endLUW	
 		end
 		
 		#File.delete(File.expand_path("~/metatest_mb.db"))
