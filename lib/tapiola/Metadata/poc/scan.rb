f574db5bd7b08d0b25c8b08be1696138bbdea345
James Wyper <jrwyper@yahoo.co.uk>
2020-02-09 18:30:29 +0000

    commit

--- lib/tapiola/Metadata/poc/scan.rb
+++ lib/tapiola/Metadata/poc/scan.rb
@@ -59,4 +59,68 @@ config.dump
 
 t = MusicFiles::Tree.new(config.root,config.group)
 
-puts "break"
+##TODO - read in matchreport if present and bypass entries
+
+t.findByOffsets
+t.findByAcoustID
+
+#TODO - make this next section group-specific
+
+=begin
+$log.info "Removing old data"
+Model::Disc.delete_all
+Model::MediumOffsetCandidate.delete_all
+Model::File.delete_all
+Model::MediumAcoustCandidate.delete_all
+Model::Tag.delete_all
+
+
+$log.info "Saving unfound"
+
+ActiveRecord::Base.transaction do
+	t.notFoundDiscs.each do |d| 
+		md = Model::Disc.create(pathname: d.pathname, number: d.number)
+		d.tracks.each_value do |f|
+			mf = md.file.create(pathname: f.pathname, basename: f.basename, track: f.track)
+			f.tags.each do |name, value|
+				mf.tag.create(name: name, value: value)
+			end
+		end
+	end
+end
+
+$log.info "Saving foundViaOffsets"
+
+ActiveRecord::Base.transaction do
+	t.foundDiscsViaOffsets.each do |d|
+		md = Model::Disc.create(pathname: d.pathname, number: d.number)
+		d.tracks.each_value do |f|
+			mf = md.file.create(pathname: f.pathname, basename: f.basename, track: f.track)
+			f.tags.each do |name, value|
+				mf.tag.create(name: name, value: value)
+			end
+		end
+		d.mediumCandidatesOffsets.each do |mc|
+			md.mediumOffsetCandidate.create(medium_id: mc.id)
+		end
+	end
+end
+
+$log.info "Saving foundViaAcoustID"
+
+ActiveRecord::Base.transaction do
+	t.foundDiscsViaAcoustID.each do |d|
+		md = Model::Disc.create(pathname: d.pathname, number: d.number)
+		d.tracks.each_value do |f|
+			mf = md.file.create(pathname: f.pathname, basename: f.basename, track: f.track)
+			f.tags.each do |name, value|
+				mf.tag.create(name: name, value: value)
+			end
+		end
+		d.mediumCandidatesAcoustID.each do |mc|
+			md.mediumAcoustCandidate.create(medium_id: mc.id)
+		end
+	end
+end
+=end
+$log.info "done"
