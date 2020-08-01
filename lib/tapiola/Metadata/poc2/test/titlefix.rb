526616755ce72ad3a51ee00fcfcc6fec39289dc1
James Wyper <jrwyper@yahoo.co.uk>
2020-08-01 22:50:36 +0100

    titlefix

--- lib/tapiola/Metadata/poc2/test/titlefix.rb
+++ lib/tapiola/Metadata/poc2/test/titlefix.rb
@@ -1,10 +1,14 @@
 require 'csv'
 require_relative '../tag.rb'
 
-CSV.foreach("/media/james/karelia/Code/titlefix.txt",{:col_sep => "\t", :encoding => "ISO8859-1"}) do |row|
+CSV.foreach("/home/james/Code/titlefix2.txt",{:col_sep => "\t", :external_encoding => "Windows-1252", :internal_encoding => "UTF-8"}) do |row|
     file = row[0] + "/" + row[1]
     newtit = row[3]
     #file.gsub!('/home/james','/media/james/karelia')
+    begin
     m = GenericTag::Metadata.from_flac(file)
-    puts "#{file}|#{newtit}|#{m.album}" 
+    #puts "#{file}|#{newtit}|#{m.album}" 
+    rescue
+    puts "#{file}"
+    end
 end
